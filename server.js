f475d9f2fb1035f47d31fde96d19f807831f13c4
rajatkakkartelus-art <rajatkakkartelus@gmail.com>
2025-09-06 05:25:54 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "tiny-messenger-server",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "commonjs",
+  "scripts": { "start": "node server.js" },
+  "dependencies": {
+    "express": "^4.19.2",
+    "socket.io": "^4.7.5"
+  },
+  "engines": { "node": ">=18" }
+}
